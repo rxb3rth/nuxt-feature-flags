34de6ae33f22788356f99a2bb7b319956750e599
Roberth González <roberth.gonzalez@manifiesto.biz>
2025-02-22 21:03:14 +0100

    feat: update getFlags function to return nested feature flags structure

--- src/runtime/server/utils.ts
+++ src/runtime/server/utils.ts
@@ -1,11 +1,6 @@
-import type { H3Event, H3EventContext } from 'h3'
 import { useRuntimeConfig } from 'nitropack/runtime'
 
 export function getFlags() {
   const runtimeConfig = useRuntimeConfig()
-  return runtimeConfig.public?.featureFlags || {}
-}
-
-export async function getContext(event?: H3Event): Promise<H3EventContext> {
-  return event?.context
+  return runtimeConfig.public?.featureFlags.flags || {}
 }
