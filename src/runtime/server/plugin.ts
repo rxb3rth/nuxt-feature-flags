9dacb95ca3dffbc17012766d81d15d4de66efa3b
Roberth González <roberth.gonzalez@manifiesto.biz>
2025-02-22 21:03:09 +0100

    feat: simplify flag resolution in nitro plugin by removing context handling

--- src/runtime/server/plugin.ts
+++ src/runtime/server/plugin.ts
@@ -1,14 +1,12 @@
 import type { NitroApp } from 'nitropack/types'
 import { defineNitroPlugin } from 'nitropack/runtime'
-import { defu } from 'defu'
 import { resolveFlags } from '../core'
-import { getFlags, getContext } from './utils'
+import { getFlags } from './utils'
 
 export default defineNitroPlugin((nitroApp: NitroApp) => {
   nitroApp.hooks.hook('request', async (event) => {
     const flagDefinitions = getFlags() || {}
-    const evaluatedFlags = resolveFlags(flagDefinitions)
-    const context = await getContext(event)
-    event.context.flags = defu(evaluatedFlags, context.flags || {})
+    const evaluatedFlags = await resolveFlags(flagDefinitions)
+    event.context.flags = evaluatedFlags
   })
 })
