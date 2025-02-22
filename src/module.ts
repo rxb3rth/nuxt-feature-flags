6a1ea44ed2fe0550aaa20a40787b6c1ffc293df0
Roberth González <roberth.gonzalez@manifiesto.biz>
2025-02-22 18:07:40 +0100

    feat: enhance feature flags module with caching and dynamic resolution from config file

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,28 @@
-import { defineNuxtModule, createResolver, addImports, addPlugin, addServerPlugin } from '@nuxt/kit'
+import { defineNuxtModule, createResolver, addImports, addPlugin, addServerPlugin, resolveAlias } from '@nuxt/kit'
 import { defu } from 'defu'
-import type { FeatureFlagsConfig } from './runtime/types'
+import type { FeatureFlagsConfig, Flag } from './runtime/types'
+
+const cache = new Map<string, Record<string, Flag>>()
+
+async function resolveDefinitions(featureFlagsConfig: FeatureFlagsConfig) {
+  if (featureFlagsConfig.inherit && featureFlagsConfig.config) {
+    try {
+      const url = resolveAlias(featureFlagsConfig.config)
+      const configFn = await import(url)
+      const definitionsFromFile = configFn.default()
+
+      const cacheKey = JSON.stringify(definitionsFromFile)
+      if (cache.has(cacheKey)) {
+        return cache.get(cacheKey)
+      }
+
+      return definitionsFromFile
+    }
+    catch (error) {
+      console.error('Failed to load feature flags from config file', error)
+    }
+  }
+}
 
 export default defineNuxtModule<FeatureFlagsConfig>({
   meta: {
@@ -11,12 +33,14 @@ export default defineNuxtModule<FeatureFlagsConfig>({
     },
     configKey: 'featureFlags',
   },
-  defaults: {},
-  setup(options, nuxt) {
+  defaults: {
+    inherit: false,
+  },
+  async setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     nuxt.options.runtimeConfig.public.featureFlags = defu(
-      nuxt.options.runtimeConfig.public.featureFlags, options as FeatureFlagsConfig)
+      nuxt.options.runtimeConfig.public.featureFlags, options)
 
     nuxt.options.nitro = nuxt.options.nitro || {}
     nuxt.options.nitro.imports = nuxt.options.nitro.imports || {}
@@ -35,6 +59,20 @@ export default defineNuxtModule<FeatureFlagsConfig>({
     addImports({
       name: 'useClientFlags',
       from: resolver.resolve('./runtime/composables'),
+    })
+
+    addImports({
+      name: 'defineFlagsConfig',
+      from: resolver.resolve('./runtime/core/define-config'),
+    })
+
+    nuxt.hook('modules:done', async () => {
+      const { config, inherit } = nuxt.options.runtimeConfig.public.featureFlags
+      if ((config && typeof config === 'string') && inherit) {
+        const definitions = await resolveDefinitions(nuxt.options.runtimeConfig.public.featureFlags)
+        nuxt.options.runtimeConfig.public.featureFlags.flags = defu(
+          nuxt.options.runtimeConfig.public.featureFlags.flags, definitions)
+      }
     })
   },
 })
