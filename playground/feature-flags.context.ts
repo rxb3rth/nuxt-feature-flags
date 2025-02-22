437815c2b44a254b794dd1317224efebea3ee199
rxb3rth <reliutg@gmail.com>
2025-02-22 14:40:16 +0100

    feat: remove unused feature flags context file

--- playground/feature-flags.context.ts
+++
@@ -1,9 +0,0 @@
-import { type H3Event, parseCookies } from 'h3'
-
-export default function featureFlagsContext(event: H3Event) {
-  return {
-    user: event?.context.user,
-    cookies: parseCookies(event),
-    environment: process.env.NODE_ENV,
-  }
-}
