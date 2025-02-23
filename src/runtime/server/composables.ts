03524a9b85618fb3cd935c93d4c029af0bb7d71d
Roberth González <reliutg@gmail.com>
2025-02-23 21:34:09 +0100

    feat: refactor server flags handling to support dynamic configuration loading and remove obsolete plugin

--- src/runtime/server/composables.ts
+++ src/runtime/server/composables.ts
@@ -1,8 +1,31 @@
-import type { H3Event } from 'h3'
+import type { H3Event, H3EventContext } from 'h3'
+import { createJiti } from 'jiti'
+import { useRuntimeConfig } from 'nitropack/runtime'
 import type { Flag, FlagDefinition } from '../types'
 
-export function useServerFlags<T extends FlagDefinition>(event: H3Event) {
-  const flags = event.context.flags || {}
+async function getFlags(context?: H3EventContext) {
+  const runtimeConfig = useRuntimeConfig()
+
+  if (!runtimeConfig._feature_flags_config_path) {
+    return context?.public?.featureFlags?.flags || {}
+  }
+
+  try {
+    const jiti = createJiti(runtimeConfig._feature_flags_config_path, {
+      interopDefault: true,
+      moduleCache: true,
+    })
+    const configFn = await jiti.import<(context: H3EventContext) => FlagDefinition>(runtimeConfig._feature_flags_config_path, { default: true })
+    return configFn(context)
+  }
+  catch (error) {
+    console.error('Error loading config file', error)
+    return {}
+  }
+}
+
+export async function useServerFlags<T extends FlagDefinition>(event: H3Event) {
+  const flags = await getFlags(event.context)
 
   return {
     flags,
