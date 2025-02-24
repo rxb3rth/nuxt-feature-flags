af0060c41439bbd67fbc9f00daaf2fa6ea153f12
Roberth González <reliutg@gmail.com>
2025-02-24 04:09:26 +0100

    refactor: update flag handling to improve type safety and simplify isEnabled checks

--- playground/server/api/dashboard.get.ts
+++ playground/server/api/dashboard.get.ts
@@ -8,7 +8,7 @@ export default defineEventHandler(async (event) => {
   const { isEnabled, flags } = await useServerFlags<FlagsSchema>(event)
   console.log('New request: ' + getRequestURL(event))
   console.log('newDashboard.isEnabled', isEnabled('newDashboard'))
-  console.log('flags', isEnabled('i'))
+  console.log('isAdmin', isEnabled('isAdmin'))
 
   return flags
 })
