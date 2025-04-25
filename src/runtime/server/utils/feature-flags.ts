9c848e0453e3119fdf009bc2643dff58306a92d9
rxb3rth <reliutg@gmail.com>
2025-04-25 12:52:22 +0200

    fix: ensure flags are initialized to an empty object in getFeatureFlags

--- src/runtime/server/utils/feature-flags.ts
+++ src/runtime/server/utils/feature-flags.ts
@@ -3,7 +3,7 @@ import featureFlagConfig from '#feature-flags/config'
 import type { FlagsSchema } from '#feature-flags/types'
 
 export function getFeatureFlags(event: H3Event) {
-  const flags = featureFlagConfig(event.context) as FlagsSchema
+  const { flags = {} } = featureFlagConfig(event.context) as FlagsSchema
 
   return {
     flags,
